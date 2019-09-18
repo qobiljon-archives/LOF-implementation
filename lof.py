7057432de156e5c9373c42952d49dc4ec12f842a
Kobiljon Toshnazarov (Kevin) <qobiljon.toshnazarov@gmail.com>
2019-09-18 17:21:28 +0900

    Update lof.py

--- lof.py
+++ lof.py
@@ -1,169 +1,111 @@
 # -*- coding: utf8 -*-
-import warnings
+import math
+from numbers import Number
 
 
-def distance_euclidean(instance1, instance2):
-    """Computes the distance between two instances. Instances should be tuples of equal length.
-    Returns: Euclidean distance
-    Signature: ((attr_1_1, attr_1_2, ...), (attr_2_1, attr_2_2, ...)) -> float"""
+def calc_distance_euclidean(vector1, vector2):
+    # !!! all passed vector elements to this method must be float values !!!
 
-    def detect_value_type(attribute):
-        """Detects the value type (number or non-number).
-        Returns: (value type, value casted as detected type)
-        Signature: value -> (str or float type, str or float value)"""
-        from numbers import Number
-        attribute_type = None
-        if isinstance(attribute, Number):
-            attribute_type = float
-            attribute = float(attribute)
-        else:
-            attribute_type = str
-            attribute = str(attribute)
-        return attribute_type, attribute
+    # validate combarability
+    if len(vector1) != len(vector2):
+        raise AttributeError("Compared vectors have different number of arguments!")
 
-    # check if instances are of same length
-    if len(instance1) != len(instance2):
-        raise AttributeError("Instances have different number of arguments.")
     # init differences vector
-    differences = [0] * len(instance1)
-    # compute difference for each attribute and store it to differences vector
-    for i, (attr1, attr2) in enumerate(zip(instance1, instance2)):
-        type1, attr1 = detect_value_type(attr1)
-        type2, attr2 = detect_value_type(attr2)
-        # raise error is attributes are not of same data type.
-        if type1 != type2:
-            raise AttributeError("Instances have different data types.")
-        if type1 is float:
-            # compute difference for float
-            differences[i] = attr1 - attr2
-        else:
-            # compute difference for string
-            if attr1 == attr2:
-                differences[i] = 0
-            else:
-                differences[i] = 1
+    per_element_distances = [0] * len(vector1)
+    
+    # compute (each vector element) difference for RMSE (for euclidean distance)
+    for index, (value1, value2) in enumerate(zip(vector1, vector2)):
+        per_element_distances[index] = value1 - value2
+
     # compute RMSE (root mean squared error)
-    rmse = (sum(map(lambda x: x ** 2, differences)) / len(differences)) ** 0.5
-    return rmse
+    return math.sqrt(sum([val**2 for val in per_element_distances]) / len(per_element_distances))
 
 
-class LOF:
-    """Helper class for performing LOF computations and instances normalization."""
-
-    def __init__(self, instances, normalize=True, distance_function=distance_euclidean):
-        self.instances = instances
-        self.normalize = normalize
-        self.distance_function = distance_function
-        if normalize:
-            self.normalize_instances()
-
-    def compute_instance_attribute_bounds(self):
-        min_values = [float("inf")] * len(self.instances[0])  # n.ones(len(self.instances[0])) * n.inf
-        max_values = [float("-inf")] * len(self.instances[0])  # n.ones(len(self.instances[0])) * -1 * n.inf
-        for instance in self.instances:
-            min_values = tuple(map(lambda x, y: min(x, y), min_values, instance))  # n.minimum(min_values, instance)
-            max_values = tuple(map(lambda x, y: max(x, y), max_values, instance))  # n.maximum(max_values, instance)
-
-        diff = [dim_max - dim_min for dim_max, dim_min in zip(max_values, min_values)]
-        if not all(diff):
-            problematic_dimensions = ", ".join(str(i + 1) for i, v in enumerate(diff) if v == 0)
-            warnings.warn("No data variation in dimensions: %s. You should check your data or disable normalization." % problematic_dimensions)
-
-        self.max_attribute_values = max_values
-        self.min_attribute_values = min_values
-
-    def normalize_instances(self):
-        """Normalizes the instances and stores the information for rescaling new instances."""
-        if not hasattr(self, "max_attribute_values"):
-            self.compute_instance_attribute_bounds()
-        new_instances = []
-        for instance in self.instances:
-            new_instances.append(self.normalize_instance(instance))  # (instance - min_values) / (max_values - min_values)
-        self.instances = new_instances
-
-    def normalize_instance(self, instance):
-        return tuple(map(lambda value, max, min: (value - min) / (max - min) if max - min > 0 else 0, instance, self.max_attribute_values, self.min_attribute_values))
-
-    def local_outlier_factor(self, min_pts, instance):
-        """The (local) outlier factor of instance captures the degree to which we call instance an outlier.
-        min_pts is a parameter that is specifying a minimum number of instances to consider for computing LOF value.
-        Returns: local outlier factor
-        Signature: (int, (attr1, attr2, ...), ((attr_1_1, ...),(attr_2_1, ...), ...)) -> float"""
-        if self.normalize:
-            instance = self.normalize_instance(instance)
-        return local_outlier_factor(min_pts, instance, self.instances, distance_function=self.distance_function)
-
-
-def k_distance(k, instance, instances, distance_function=distance_euclidean):
-    # TODO: implement caching
-    """Computes the k-distance of instance as defined in paper. It also gatheres the set of k-distance neighbours.
-    Returns: (k-distance, k-distance neighbours)
-    Signature: (int, (attr1, attr2, ...), ((attr_1_1, ...),(attr_2_1, ...), ...)) -> (float, ((attr_j_1, ...),(attr_k_1, ...), ...))"""
+def calc_k_distance(k, vector, dataset):
+    # TODO: consider caching for more efficient re-computation
     distances = {}
-    for instance2 in instances:
-        distance_value = distance_function(instance, instance2)
-        if distance_value in distances:
-            distances[distance_value].append(instance2)
+    for vector2 in dataset:
+        distance = calc_distance_euclidean(vector1=vector, vector2=vector2)
+        if distance in distances:
+            distances[distance].append(vector2)
         else:
-            distances[distance_value] = [instance2]
+            distances[distance] = [vector2]
     distances = sorted(distances.items())
     neighbours = []
     [neighbours.extend(n[1]) for n in distances[:k]]
-    k_distance_value = distances[k - 1][0] if len(distances) >= k else distances[-1][0]
-    return k_distance_value, neighbours
+    k_distance = distances[k - 1][0] if len(distances) >= k else distances[-1][0]
+    return k_distance, neighbours
 
 
-def k_reach_distance(k, instance1, instance2, instances, distance_function=distance_euclidean):
-    """The reachability distance of instance1 with respect to instance2.
-    Returns: reachability distance
-    Signature: (int, (attr_1_1, ...),(attr_2_1, ...)) -> float"""
-    (k_distance_value, neighbours) = k_distance(k, instance2, instances, distance_function=distance_function)
-    return max([k_distance_value, distance_function(instance1, instance2)])
+def calc_k_reachability_distance(k, vector1, vector2, dataset):
+    (k_distance, neighbours) = calc_k_distance(k, vector2, dataset)
+    return max(k_distance, calc_distance_euclidean(vector1=vector1, vector2=vector2))
 
 
-def local_reach_density(min_pts, instance, instances, **kwargs):
-    """Local reachability density of instance is the inverse of the average reachability
-    distance based on the min_pts-nearest neighbors of instance.
-    Returns: local reachability density
-    Signature: (int, (attr1, attr2, ...), ((attr_1_1, ...),(attr_2_1, ...), ...)) -> float"""
-    (k_distance_value, neighbours) = k_distance(min_pts, instance, instances, **kwargs)
-    reachability_distances_array = [0] * len(neighbours)  # n.zeros(len(neighbours))
-    for i, neighbour in enumerate(neighbours):
-        reachability_distances_array[i] = k_reach_distance(min_pts, instance, neighbour, instances, **kwargs)
-    if not any(reachability_distances_array):
-        warnings.warn("Instance %s (could be normalized) is identical to all the neighbors. Setting local reachability density to inf." % repr(instance))
+def calc_local_reachability_density(k, vector, dataset):
+    (k_distance, neighbours) = calc_k_distance(k=k, vector=vector, dataset=dataset)
+    reachability_distances = [0] * len(neighbours)
+    for index, neighbour in enumerate(neighbours):
+        reachability_distances[index] = calc_k_reachability_distance(k=k, vector1=vector, vector2=neighbour, dataset=dataset)
+    if sum(reachability_distances) == 0:
+        # TODO: vector is identical with its neighbors, consider fixing this case!
+        # returning 'inf' to note that this vector has an issue
         return float("inf")
     else:
-        return len(neighbours) / sum(reachability_distances_array)
+        return len(neighbours) / sum(reachability_distances)
 
 
-def local_outlier_factor(min_pts, instance, instances, **kwargs):
-    """The (local) outlier factor of instance captures the degree to which we call instance an outlier.
-    min_pts is a parameter that is specifying a minimum number of instances to consider for computing LOF value.
-    Returns: local outlier factor
-    Signature: (int, (attr1, attr2, ...), ((attr_1_1, ...),(attr_2_1, ...), ...)) -> float"""
-    (k_distance_value, neighbours) = k_distance(min_pts, instance, instances, **kwargs)
-    instance_lrd = local_reach_density(min_pts, instance, instances, **kwargs)
-    lrd_ratios_array = [0] * len(neighbours)
-    for i, neighbour in enumerate(neighbours):
-        instances_without_instance = set(instances)
-        instances_without_instance.discard(neighbour)
-        neighbour_lrd = local_reach_density(min_pts, neighbour, instances_without_instance, **kwargs)
-        lrd_ratios_array[i] = neighbour_lrd / instance_lrd
-    return sum(lrd_ratios_array) / len(neighbours)
+def calc_local_outlier_factor(k, vector, dataset):
+    (k_distance, neighbours) = calc_k_distance(k=k, vector=vector, dataset=dataset)
+    vector_lrd = calc_local_reachability_density(k=k, vector=vector, dataset=dataset)
+    lrd_ratios = [0] * len(neighbours)
+    
+    for index, neighbour in enumerate(neighbours):
+        tmp_dataset_without_neighbor = set(dataset)
+        tmp_dataset_without_neighbor.discard(neighbour)
+        neighbour_lrd = calc_local_reachability_density(k=k, vector=neighbour, dataset=tmp_dataset_without_neighbor)
+        lrd_ratios[index] = neighbour_lrd / vector_lrd
+    
+    return sum(lrd_ratios) / len(neighbours)
 
 
-def outliers(k, instances, **kwargs):
-    """Simple procedure to identify outliers in the dataset."""
-    instances_value_backup = instances
-    _outliers = []
-    for i, instance in enumerate(instances_value_backup):
-        instances = list(instances_value_backup)
-        instances.remove(instance)
-        _lof = LOF(instances, normalize=False, **kwargs)
-        # _lof = LOF(instances, normalize=True, **kwargs)
-        lof_value = _lof.local_outlier_factor(k, instance)
-        if lof_value > 1:
-            _outliers.append({"lof": lof_value, "instance": instance, "index": i})
-    _outliers.sort(key=lambda o: o["lof"], reverse=True)
-    return _outliers
+def calc_lof_boundary(vector, dataset):
+    return 0
+
+
+def detect_outliers(k, dataset):
+    tmp_dataset = dataset
+    outliers = []
+
+    for index, vector in enumerate(tmp_dataset):
+        tmp_dataset_without_vector = list(tmp_dataset)
+        tmp_dataset_without_vector.remove(vector)
+        LOF = calc_local_outlier_factor(k, vector, tmp_dataset_without_vector)
+        
+        # TODO: add threshold in LOF boundary calculation
+        # According to the source: https://www.dbs.ifi.lmu.de/Publikationen/Papers/LOF.pdf
+        # Theorem 1: LOF(p) <= lof_boundary(p): direct_max_distance(p) / indirect_min_distance(p)
+        # But it needs to be implemented after distance calculation caching is implemented
+        if LOF > calc_lof_boundary(vector, dataset):
+            outliers.append({"LOF": LOF, "vector": vector, "index": index})
+    outliers.sort(key=lambda lof_key: lof_key["LOF"], reverse=True)
+    return outliers
+
+
+if __name__ == "__main__":
+    # dataset = (
+    #     (31, 340, 56, 15),
+    #     (26, 376, 80, 3.7),
+    #     (26, 380, 72, 5.1),
+    #     (25, 368, 75, 3.9),
+    #     (25, 370, 68, 5.5)
+    # )
+    dataset = (
+        (0, 0, 0),
+        (-0.07858112, 0.04371652, 0.08754063),
+        (-0.071033, 0.04098192, 0.06599712),
+        (-0.068174735, 0.026259795, 0.06360817),
+        (-0.062882856, 0.030693293, 0.07797909)
+    )
+    for outlier in detect_outliers(3, dataset):
+        print(outlier)
